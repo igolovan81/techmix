f1a90aa5c06cc0d0d9cbeadcfa58994497b7411c
Igor Golovan <Igor.Golovan@aerlingus.com>
2026-06-10 11:24:28 +0200

    feat: add springdoc dependency and OpenAPI metadata

--- message-brokers/rabbitmq/spring-demo/src/main/java/com/testingai/rabbitmq/RabbitMqDemoApplication.java
+++ message-brokers/rabbitmq/spring-demo/src/main/java/com/testingai/rabbitmq/RabbitMqDemoApplication.java
@@ -1,8 +1,15 @@
 package com.testingai.rabbitmq;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+        title = "RabbitMQ Demo API",
+        version = "1.0.0",
+        description = "Learning project demonstrating RabbitMQ messaging patterns"
+))
 @SpringBootApplication
 public class RabbitMqDemoApplication {
 
