8d9006426c20ad8b214b4de6b059c35e780a8224
Igor Golovan <Igor.Golovan@aerlingus.com>
2026-06-11 11:16:27 +0200

    feat: add 5s TTL to simple queue

--- message-brokers/rabbitmq/spring-demo/src/main/java/com/testingai/rabbitmq/config/SimpleQueueConfig.java
+++ message-brokers/rabbitmq/spring-demo/src/main/java/com/testingai/rabbitmq/config/SimpleQueueConfig.java
@@ -2,6 +2,7 @@ package com.testingai.rabbitmq.config;
 
 import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +12,11 @@ import org.springframework.context.annotation.Configuration;
 public class SimpleQueueConfig {
 
     public static final String QUEUE_NAME = "simple.queue";
+    public static final int MESSAGE_TTL_MS = 5000;
 
     @Bean
     public Queue simpleQueue() {
-        return new Queue(QUEUE_NAME, true);
+        return QueueBuilder.durable(QUEUE_NAME).ttl(MESSAGE_TTL_MS).build();
     }
 
     @Bean
---
+++ message-brokers/rabbitmq/spring-demo/src/test/java/com/testingai/rabbitmq/config/SimpleQueueConfigTest.java
@@ -0,0 +1,17 @@
+package com.testingai.rabbitmq.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.Queue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleQueueConfigTest {
+
+    private final SimpleQueueConfig config = new SimpleQueueConfig();
+
+    @Test
+    void simpleQueue_shouldHaveTtlOf5000ms() {
+        Queue queue = config.simpleQueue();
+        assertThat(queue.getArguments()).containsEntry("x-message-ttl", 5000);
+    }
+}
