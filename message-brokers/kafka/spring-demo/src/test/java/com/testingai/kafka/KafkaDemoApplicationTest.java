ba8a7c2e127296095c57bd6ee0014a9529d5d9b0
Igor Golovan <Igor.Golovan@aerlingus.com>
2026-06-11 13:34:31 +0200

    feat: add Kafka Spring Boot project skeleton

---
+++ message-brokers/kafka/spring-demo/src/test/java/com/testingai/kafka/KafkaDemoApplicationTest.java
@@ -0,0 +1,11 @@
+package com.testingai.kafka;
+
+import org.junit.jupiter.api.Test;
+
+class KafkaDemoApplicationTest {
+
+    @Test
+    void mainClassExists() {
+        new KafkaDemoApplication();
+    }
+}
