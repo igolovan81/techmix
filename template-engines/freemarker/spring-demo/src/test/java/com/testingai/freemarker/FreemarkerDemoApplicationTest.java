5de78eae7172dfad4e0d37529a2cca619d261d9b
Igor Golovan <Igor.Golovan@aerlingus.com>
2026-07-13 09:19:27 +0200

    feat(freemarker): scaffold freemarker-demo module

---
+++ template-engines/freemarker/spring-demo/src/test/java/com/testingai/freemarker/FreemarkerDemoApplicationTest.java
@@ -0,0 +1,11 @@
+package com.testingai.freemarker;
+
+import org.junit.jupiter.api.Test;
+
+class FreemarkerDemoApplicationTest {
+
+	@Test
+	void mainClassExists() {
+		new FreemarkerDemoApplication();
+	}
+}
