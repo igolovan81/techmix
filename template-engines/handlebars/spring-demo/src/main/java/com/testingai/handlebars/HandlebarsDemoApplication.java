98b351e0ff80a424ff7b574caeef45aad13f1dcc
Igor Golovan <Igor.Golovan@aerlingus.com>
2026-07-13 09:07:52 +0200

    feat(handlebars): scaffold handlebars-demo module

---
+++ template-engines/handlebars/spring-demo/src/main/java/com/testingai/handlebars/HandlebarsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.testingai.handlebars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlebarsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlebarsDemoApplication.class, args);
+	}
+}
---
+++ template-engines/handlebars/spring-demo/src/test/java/com/testingai/handlebars/HandlebarsDemoApplicationTest.java
@@ -0,0 +1,11 @@
+package com.testingai.handlebars;
+
+import org.junit.jupiter.api.Test;
+
+class HandlebarsDemoApplicationTest {
+
+	@Test
+	void mainClassExists() {
+		new HandlebarsDemoApplication();
+	}
+}
