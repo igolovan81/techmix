b48c704a92a996457e6501bb838f0e3749deebea
Igor Golovan <Igor.Golovan@aerlingus.com>
2026-07-29 11:56:58 +0200

    feat(communication-protocols): scaffold graphql-spring-demo module

---
+++ communication-protocols/graphql/spring-demo/src/main/java/com/testingai/graphql/GraphQlSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.testingai.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlSpringDemoApplication.class, args);
+	}
+}
---
+++ communication-protocols/graphql/spring-demo/src/test/java/com/testingai/graphql/GraphQlSpringDemoApplicationTest.java
@@ -0,0 +1,12 @@
+package com.testingai.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlSpringDemoApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
