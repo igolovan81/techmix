afef9fbba09354da448b050ba62561db21982065
Igor Golovan <Igor.Golovan@aerlingus.com>
2026-07-30 11:40:31 +0200

    test(communication-protocols): authenticate addReview in the GraphQL load tests

--- communication-protocols/graphql/spring-demo/src/test/java/com/testingai/graphql/performance/DemoSimulation.java
+++ communication-protocols/graphql/spring-demo/src/test/java/com/testingai/graphql/performance/DemoSimulation.java
@@ -4,7 +4,9 @@ import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.Base64;
 
 import static io.gatling.javaapi.core.CoreDsl.StringBody;
 import static io.gatling.javaapi.core.CoreDsl.rampUsers;
@@ -23,6 +25,9 @@ public class DemoSimulation extends Simulation {
 	private static final String ADD_REVIEW_MUTATION_BODY = """
 			{"query":"mutation { addReview(input: { productId: \\"p1\\", author: \\"LoadTest\\", rating: 5, comment: \\"Load test review\\" }) { id } }"}""";
 
+	private static final String USER_BASIC_AUTH_HEADER = "Basic "
+			+ Base64.getEncoder().encodeToString("user:userPassword".getBytes(StandardCharsets.UTF_8));
+
 	private final HttpProtocolBuilder httpProtocol = http.baseUrl("http://localhost:8092")
 			.contentTypeHeader("application/json");
 
@@ -32,7 +37,8 @@ public class DemoSimulation extends Simulation {
 			.pause(Duration.ofMillis(500))
 			.exec(http("Query - Product by Id").post("/graphql").body(StringBody(PRODUCT_QUERY_BODY))
 					.check(status().is(200)))
-			.pause(Duration.ofMillis(500)).exec(http("Mutation - Add Review").post("/graphql")
+			.pause(Duration.ofMillis(500))
+			.exec(http("Mutation - Add Review").post("/graphql").header("Authorization", USER_BASIC_AUTH_HEADER)
 					.body(StringBody(ADD_REVIEW_MUTATION_BODY)).check(status().is(200)));
 
 	{
