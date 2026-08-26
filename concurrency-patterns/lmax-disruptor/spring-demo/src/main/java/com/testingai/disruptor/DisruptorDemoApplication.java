b665066efebcbd081f1ac01ea6d0d08199988bfd
Igor Golovan <Igor.Golovan@aerlingus.com>
2026-08-26 11:59:38 +0200

    feat(concurrency-patterns): scaffold lmax-disruptor-demo module

---
+++ concurrency-patterns/lmax-disruptor/spring-demo/src/main/java/com/testingai/disruptor/DisruptorDemoApplication.java
@@ -0,0 +1,12 @@
+package com.testingai.disruptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisruptorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisruptorDemoApplication.class, args);
+	}
+}
---
+++ concurrency-patterns/lmax-disruptor/spring-demo/src/test/java/com/testingai/disruptor/DisruptorDemoApplicationTest.java
@@ -0,0 +1,12 @@
+package com.testingai.disruptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisruptorDemoApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
